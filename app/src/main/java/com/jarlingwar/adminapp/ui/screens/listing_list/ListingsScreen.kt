0e610cb5e0d49662aef62b4cc13df667d02c339b
igor <igorek1955@gmail.com>
2023-11-20 15:54:06 +0300

    - log dialog ListingsScreen fix

--- app/src/main/java/com/jarlingwar/adminapp/ui/screens/listing_list/ListingsScreen.kt
+++ app/src/main/java/com/jarlingwar/adminapp/ui/screens/listing_list/ListingsScreen.kt
@@ -97,83 +97,85 @@ fun ListingsScreen(
         currentDestination = currentDestination,
         onNavigate = onNavigate
     ) { paddingValues ->
-        Column(
-            Modifier
-                .padding(paddingValues)
-                .fillMaxWidth()
-                .padding(top = 10.dp)
-                .paddingPrimaryStartEnd()
-        ) {
-            val sortOrder = SortOrder.values().map { stringResource(id = it.titleResId) }
-            val countries = CountryInfo.values().map { stringResource(id = it.titleResId) }
-
-            Row(
+        Box {
+            Column(
                 Modifier
+                    .padding(paddingValues)
                     .fillMaxWidth()
-                    .padding(bottom = 20.dp)
+                    .padding(top = 10.dp)
+                    .paddingPrimaryStartEnd()
             ) {
-                DropDownTextMenu(
-                    Modifier.weight(0.5f), values = sortOrder, label = stringResource(
-                        id = R.string.sorting
-                    ),
-                    defaultVal = stringResource(id = viewModel.params.orderBy!!.titleResId)
-                ) { viewModel.updateSortOrder(it) }
-                Spacer(modifier = Modifier.width(2.dp))
-                DropDownTextMenu(
-                    Modifier.weight(0.5f), values = countries, label = stringResource(
-                        id = R.string.country
-                    ),
-                    defaultVal = stringResource(id = viewModel.params.country!!.titleResId)
-                ) { viewModel.updateCountry(it) }
-            }
+                val sortOrder = SortOrder.values().map { stringResource(id = it.titleResId) }
+                val countries = CountryInfo.values().map { stringResource(id = it.titleResId) }
 
-            Box(Modifier.pullRefresh(pullRefreshState)) {
-                if (pullRefreshState.progress > 1.0f) {
-                    PullRefreshIndicator(
-                        backgroundColor = MaterialTheme.adminColors.backgroundPrimary,
-                        refreshing = viewModel.isRefreshing,
-                        state = pullRefreshState,
-                        modifier = Modifier
-                            .align(Alignment.TopCenter)
-                            .zIndex(1f)
-                    )
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp)
+                ) {
+                    DropDownTextMenu(
+                        Modifier.weight(0.5f), values = sortOrder, label = stringResource(
+                            id = R.string.sorting
+                        ),
+                        defaultVal = stringResource(id = viewModel.params.orderBy!!.titleResId)
+                    ) { viewModel.updateSortOrder(it) }
+                    Spacer(modifier = Modifier.width(2.dp))
+                    DropDownTextMenu(
+                        Modifier.weight(0.5f), values = countries, label = stringResource(
+                            id = R.string.country
+                        ),
+                        defaultVal = stringResource(id = viewModel.params.country!!.titleResId)
+                    ) { viewModel.updateCountry(it) }
                 }
-                if (viewModel.listings.isEmpty() && !viewModel.isLoading) {
-                    NoResults()
-                } else {
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(2),
-                        horizontalArrangement = Arrangement.spacedBy(10.dp),
-                        verticalArrangement = Arrangement.spacedBy(10.dp)
-                    ) {
-                        itemsIndexed(
-                            items = viewModel.listings,
-                            key = { _, item -> item.listingId }
-                        ) { index, item ->
-                            ListingItem(listing = item) {
-                                sharedViewModel?.selectedListing = item
-                                onNavigateToListing()
-                            }
-                            if (index == viewModel.listings.size - 5) viewModel.loadNext()
-                        }
-                        if (viewModel.isLoadingNext) {
-                            item(span = { GridItemSpan(2) }) {
-                                LoadingNextIndicator()
-                            }
-                        }
+
+                Box(Modifier.pullRefresh(pullRefreshState)) {
+                    if (pullRefreshState.progress > 1.0f) {
+                        PullRefreshIndicator(
+                            backgroundColor = MaterialTheme.adminColors.backgroundPrimary,
+                            refreshing = viewModel.isRefreshing,
+                            state = pullRefreshState,
+                            modifier = Modifier
+                                .align(Alignment.TopCenter)
+                                .zIndex(1f)
+                        )
                     }
-                    if (viewModel.isLoading && viewModel.listings.isEmpty()) {
-                        LoadingIndicator()
-                    }
-                    viewModel.error?.showSnack { viewModel.error = null }
-                    if (showNoPermSnackbar) {
-                        MySnack(text = "Can't send notifications") { showNoPermSnackbar = false }
+                    if (viewModel.listings.isEmpty() && !viewModel.isLoading) {
+                        NoResults()
+                    } else {
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
+                            horizontalArrangement = Arrangement.spacedBy(10.dp),
+                            verticalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            itemsIndexed(
+                                items = viewModel.listings,
+                                key = { _, item -> item.listingId }
+                            ) { index, item ->
+                                ListingItem(listing = item) {
+                                    sharedViewModel?.selectedListing = item
+                                    onNavigateToListing()
+                                }
+                                if (index == viewModel.listings.size - 5) viewModel.loadNext()
+                            }
+                            if (viewModel.isLoadingNext) {
+                                item(span = { GridItemSpan(2) }) {
+                                    LoadingNextIndicator()
+                                }
+                            }
+                        }
+                        if (viewModel.isLoading && viewModel.listings.isEmpty()) {
+                            LoadingIndicator()
+                        }
+                        viewModel.error?.showSnack { viewModel.error = null }
+                        if (showNoPermSnackbar) {
+                            MySnack(text = "Can't send notifications") { showNoPermSnackbar = false }
+                        }
                     }
                 }
-                if (viewModel.logs.isNotEmpty()) {
-                    LogDialog(log = viewModel.logs) {
-                        viewModel.logs = ""
-                    }
+            }
+            if (viewModel.logs.isNotEmpty()) {
+                LogDialog(log = viewModel.logs) {
+                    viewModel.logs = ""
                 }
             }
         }
